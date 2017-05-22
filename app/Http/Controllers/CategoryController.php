70cd79084aa7efa16fa7646c0313e5d93377781a
clonehugger <andrea.puente@udem.edu>
2017-05-22 09:14:01 -0500

    category table fix

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class CategoryController extends Controller
+{
+    //
+
+    public function view()
+    {
+        return view('about.contact');
+    }
+
+    public function add()
+    {
+    }
+}
--- database/migrations/2017_05_22_141227_create_categorys_table.php
+++ database/migrations/2017_05_22_141227_create_categorys_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCatsTable extends Migration
+class CreateCategorysTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,7 +12,7 @@ class CreateCatsTable extends Migration
      */
     public function up()
     {
-      Schema::create('cats', function (Blueprint $table) {
+      Schema::create('categorys', function (Blueprint $table) {
           $table->string('user_id');
           $table->string('name');
       });
@@ -26,7 +26,7 @@ class CreateCatsTable extends Migration
      */
     public function down()
     {
-      Schema::drop('cats');
+      Schema::drop('categorys');
         //
     }
 }
